dc53ee83b6446e417cbaa32da9d9d237afd13fca
Basil <Syperia@gmail.com>
2015-12-08 15:46:09 +0200

    done

--- app/controllers/contactsController.js
+++ app/controllers/contactsController.js
@@ -10,20 +10,32 @@ angular.module("angControllers").controller("contactsController", [
         ];
 
         $scope.login = function(provider) {
+
+            if(provider.signedIn) {
+                return;
+            }
+
+            provider.requestInProgress = true;
             hello.login(provider.name).then(function (data) {
                     console.log('LOGGED IN!');
 
                     $timeout(function() {
                         provider.signedIn = true;
+                        provider.requestInProgress = false;
                     }, 300);
                 },
                 function (e) {
                     console.log('NOT LOGGED IN!');
                     console.log(e)
+                    provider.requestInProgress = false;
                 })
         };
 
         $scope.getContacts = function(provider) {
+
+            if(!provider.signedIn) {
+                return;
+            }
 
             if(provider.requestInProgress) {
                 return;
@@ -34,8 +46,10 @@ angular.module("angControllers").controller("contactsController", [
                     console.log('GOT CONTACTS!');
 
                     $timeout(function() {
-                        for (var i = 0; i < res.data.length; i++) {
-                            provider.contacts.push({ name: res.data[i].name, email: res.data[i].email });
+                        if(res.data) {
+                            for (var i = 0; i < res.data.length; i++) {
+                                provider.contacts.push({ name: res.data[i].name, email: res.data[i].email });
+                            }
                         }
                         provider.requestInProgress = false;
                     }, 300);
@@ -47,7 +61,7 @@ angular.module("angControllers").controller("contactsController", [
                 })
         };
 
-        $scope.saveContacts = function() {
+        $scope.saveContacts = function(a) {
             $scope.isSaving = true;
 
             var data = [];
@@ -64,6 +78,10 @@ angular.module("angControllers").controller("contactsController", [
                     var blob = new Blob([json], {type: "application/json"});
                     window.open(URL.createObjectURL(blob), 'contacts.json');
 
+                    //TODO: use html5 download attribute
+                    //var url  = URL.createObjectURL(blob);
+                    //a.download    = "backup.json";
+                    //a.href        = url;
                     $scope.isSaving = false;
                 },
                 function (e) {
